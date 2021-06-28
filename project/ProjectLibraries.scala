0548e15b6f6c60b2fde1791eabf1ddfbd6427160
marlowuser <lmichaelides@marlowgroup.com>
2021-06-28 15:15:19 +0300

    CA-1197: java to scala conversion

--- project/ProjectLibraries.scala
+++ project/ProjectLibraries.scala
@@ -15,7 +15,20 @@ object ProjectLibraries {
     lazy val io: Seq[ModuleID] = Seq(
       ScalaLogging,
       JodaTime,
-      TypeSafeConfig
+      TypeSafeConfig,
+      ItextpdfKernel,
+      ItextpdfIo,
+      ItextpdfLayout,
+      ItextpdfHtml2pdf,
+      ItextpdfForms,
+      ItextpdfSvg,
+      JacksonCore,
+      JacksonAnnotations,
+      JacksonDatabind,
+      Slf4jApi,
+      Slf4jSimple,
+      BcpkixJdk15on,
+      BcprovJdk15on
     ) ++ testkit
   }
 
@@ -23,6 +36,19 @@ object ProjectLibraries {
     val ScalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % ScalaLoggingVersion
     val JodaTime = "joda-time" % "joda-time" % JodaTimeVersion
     val TypeSafeConfig = "com.typesafe" % "config" % TypeSafeConfigVersion
+    val ItextpdfKernel = "com.itextpdf" % "kernel" % ItextpdfKernelVersion
+    val ItextpdfIo = "com.itextpdf" % "io" % ItextpdfIoVersion
+    val ItextpdfLayout = "com.itextpdf" % "layout" % ItextpdfLayoutVersion
+    val ItextpdfHtml2pdf = "com.itextpdf" % "html2pdf" % ItextpdfHtml2pdfVersion
+    val ItextpdfForms = "com.itextpdf" % "forms" % ItextpdfFormsVersion
+    val ItextpdfSvg = "com.itextpdf" % "svg" % ItextpdfSvgVersion
+    val JacksonCore = "com.fasterxml.jackson.core" % "jackson-core" % JacksonCoreVersion
+    val JacksonAnnotations = "com.fasterxml.jackson.core" % "jackson-annotations" % JacksonAnnotationsVersion
+    val JacksonDatabind = "com.fasterxml.jackson.core" % "jackson-databind" % JacksonDatabindVersion
+    val Slf4jApi = "org.slf4j" % "slf4j-api" % Slf4jApiVersion
+    val Slf4jSimple = "org.slf4j" % "slf4j-simple" % Slf4jSimpleVersion
+    val BcpkixJdk15on = "org.bouncycastle" % "bcpkix-jdk15on" % BcpkixJdk15onVersion
+    val BcprovJdk15on = "org.bouncycastle" % "bcprov-jdk15on" % BcprovJdk15onVersion
   }
 
   object TestLibraries {
--- src/test/scala/specs/pdf/PdfUtilsSpec.scala
+++ src/test/scala/specs/pdf/PdfUtilsSpec.scala
@@ -3,10 +3,34 @@ package specs.pdf
 import com.marlow.io.pdf.PdfUtils
 import org.specs2.mutable.Specification
 
+import java.awt.Desktop
+import java.io.File
+import java.nio.file.{Files, Paths}
+import scala.io.Source
+
 class PdfUtilsSpec extends Specification {
-  "PdfUtils" should {
-    "generate report" in {
-      PdfUtils.generateReport mustEqual "generateReport"
+  val dest = "./src/test/resources/test.pdf"
+  val source: Source = Source.fromFile("src/test/resources/test.json")
+  try {
+    val json = source.getLines().mkString
+    PdfUtils.jsonToPdf(json, dest)
+  } finally {
+    try {
+      source.close
+    } catch {
+      case e: Exception =>
     }
   }
+
+  "PdfUtils" should {
+    "generate a pdf report from json string" in {
+      Files.exists(Paths.get(dest)) mustEqual true
+      new File(dest).length() mustEqual 6154
+    }
+  }
+
+  if (Desktop.isDesktopSupported) {
+    Desktop.getDesktop.open(new File(dest))
+  }
+
 }
